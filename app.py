f7cb0a04ae0f7375b9a72efb9cc3c9e6ccaed72e
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-09-19 23:26:40 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+#print("Hello world!")
+name = "Bartek"
+age = 20
+language = "Python"
+random_text = "sam nie wiem co gadam wiec kurwa nie gadam"
+
+#print(f"My name is {name} and i have {age} years old")
+
+#print(random_text.replace("kurwa", "k****"))
+
+# if age == 20:
+#     print("masz 20 lat, zajebiscie!")
+# elif age >= 18:
+#     print("Mozesz wejsc")
+# else:
+#     print("nie mozesz wejsc")
+
+zeros_list = [0] * 15
+users = ['bartek', 'marysia', 'julka', 'dominik', 'wojtek']
+main_user, *other_users = users
+
+two_first = users[:2]
+rest = users[2:]
+
+users.append("nowy")
+
+# for i in range(5):
+#     print(users[i])
+
+num = 0
+
+while num < 15:
+    print(num)
+    if num == 8:
+        continue
+    num += 1
