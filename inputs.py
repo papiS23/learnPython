efc7aa0dd085ed517629589c5e9e9ef85b450a97
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-09-23 18:20:42 +0200

    for loop, table, import math

--- inputs.py
+++ inputs.py
@@ -1,2 +1,27 @@
-name = input("Podaj imie: ")
-print(f"Witaj {name}!")
+# num_1 = int(input("Podaj pierwsza liczbe: "))
+# action = input("Podaj dzialanie (*, /, ^, +, -): ")
+# num_2 = int(input("Podaj druga liczbe: "))
+#
+#
+# if(action == "+"):
+#     print(f"Wynik dodawania to: {num_1+num_2}")
+# elif(action == "-"):
+#     print(f"Wynik odejmowania to: {num_1-num_2}")
+# elif (action == "*"):
+#     print(f"Wynik mnożenia to: {num_1*num_2}")
+# elif (action == "/"):
+#     print(f"Wynik dzielenia to: {num_1/num_2}")
+# elif (action == "^"):
+#     print(f"Wynik potęgowania: {pow(num_1, num_2)}")
+# else:
+#     print("Niepoprawne działanie")
+
+table = ['siema', 'witamwitam', 'costatamcos']
+# for item in table:
+#     print(item)
+# for i in range(5):
+#     table.append(i)
+#     print(table)
+from math import *
+# print(floor(3.89))
+num = 321
