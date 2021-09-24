36a6e999f042ae293d949b797ea1fde3f92821f2
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-09-24 10:29:51 +0200

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -15,8 +15,21 @@
 #     print(f"Wynik potęgowania: {pow(num_1, num_2)}")
 # else:
 #     print("Niepoprawne działanie")
+some_numbers = [1, 3, 5, 7, 9, 0]
+friends = ['marysia', 'dominik', 'pawel', 'blazej', 'wojtek', 'michal']
+friends.extend(some_numbers)
+# print(friends)
+reverse_table = []
+for num in some_numbers:
+    reverse_table.insert(0, num)
 
-table = ['siema', 'witamwitam', 'costatamcos']
+print(reverse_table)
+# for i in range(6):
+#     some_numbers[i] = some_numbers[i] + 1
+# print(some_numbers)
+
+
+
 # for item in table:
 #     print(item)
 # for i in range(5):
@@ -24,4 +37,3 @@ table = ['siema', 'witamwitam', 'costatamcos']
 #     print(table)
 from math import *
 # print(floor(3.89))
-num = 321
