f493a7695f4b044b80bd07f3e736eebafb7f1c72
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-10-14 22:09:07 +0200

    jestem debilem

--- files.py
+++ files.py
@@ -1,9 +1,22 @@
-file = open('file.txt', 'r')
-f = file.readlines()
-file_words = []
-for line in f:
-    if line[-1] == "\n":
-        file_words.append(line[:-1])
-    else:
-        file_words.append(line)
-print(file_words)
+input_file = open('file.txt', 'r')
+i_f = input_file.readlines()
+life_start_list = []
+
+
+for line in i_f:
+    life_start_list.append(list(line.strip()))
+
+life_end_list = life_start_list
+
+line_nr = -1
+for life_line in life_start_list:
+    line_nr = line_nr + 1
+    for i in range(len(life_line)):
+        if life_line[i] == '1':
+            print(line_nr)
+            life_end_list[line_nr][i-1] = '1'
+            life_end_list[line_nr][i+1] = '1'
+            life_end_list[line_nr-1][i] = '1'
+            life_end_list[line_nr+1][i] = '1'
+
+print(life_end_list)
