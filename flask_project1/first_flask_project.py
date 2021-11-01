ea972b7184be9c6f78151c151f51f96ee978bf6e
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-11-01 12:49:54 +0100

    tutorial

--- flask_project1/first_flask_project.py
+++ flask_project1/first_flask_project.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": 'April 20, 2018'
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+
+    return render_template('about.html', title="About")
