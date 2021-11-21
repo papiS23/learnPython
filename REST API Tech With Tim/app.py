c0bbea2b866779e8891158518849fa01b7f31a4b
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-11-21 22:20:16 +0100

    flask api

--- REST API Tech With Tim/app.py
+++ REST API Tech With Tim/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://scott:tiger@localhost/lotnisko"
 
 names = {"bartek" : {"age" : 17, "gender" : "male"},
          "marysia" : {"age" : 18, "gender" : "female"}}
