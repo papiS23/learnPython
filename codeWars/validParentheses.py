caeb962e8d1820b6b39a1ed8d62030eab21ff4cd
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2022-02-06 18:12:57 +0100

    cos nie dziala do konca poprawnie

--- codeWars/validParentheses.py
+++ codeWars/validParentheses.py
@@ -1,13 +1,23 @@
 def valid_parentheses(string):
-    parenthases = ''
+    parenthases = []
     for char in string:
         if char == '(' or char == ")":
-            parenthases += char
-    for i in range(len(parenthases)):
-        if parenthases[i] == '(' and parenthases[-(i+1)] == ')':
-            continue
-        elif parenthases[i] == ')' and parenthases[-(i+1)] == '(':
+            parenthases.append(char)
+    counter = 0
+    while counter == 0:
+        length = len(parenthases)
+        print(parenthases)
+        for i in range(len(parenthases)-1):
+            if parenthases[i] == '(' and parenthases[i+1] == ')':
+                parenthases.remove(parenthases[i])
+                parenthases.remove(parenthases[i])
+                break
+        if len(parenthases) == length:
+            counter = 1
+    if len(parenthases) == 0:
+        return True
+    else:
+        return False
 
-
-
-## (()(()))
+## funkcja zwraca true a nie powinna, usuwane sa elementy ktore nie powinny
+print(valid_parentheses("())(()"))
